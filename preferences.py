3cb132f838c63358cdd2034f8e7f9563f8cbff2b
MrMoonKr <MrMoonKr@gmail.com>
2023-01-03 19:31:21 +0900

    bpy.types.AddonPreferences 수정

--- preferences.py
+++ preferences.py
@@ -2,7 +2,8 @@ import bpy
 
 
 class WarCraft3Preferences(bpy.types.AddonPreferences):
-    bl_idname = 'io_scene_warcraft_3'
+    bl_idname = __package__
+    
     resourceFolder: bpy.props.StringProperty(
         name='Resource',
         default='',
@@ -23,3 +24,4 @@ class WarCraft3Preferences(bpy.types.AddonPreferences):
         layout.prop(self, 'resourceFolder')
         layout.prop(self, 'alternativeResourceFolder')
         layout.prop(self, 'textureExtension')
+
