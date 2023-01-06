4f88c34696d3cd97fcf29dd976188e502c96e223
MrMoonKr <MrMoonKr@gmail.com>
2023-01-06 19:37:26 +0900

    ...

--- classes/WarCraft3Bone.py
+++ classes/WarCraft3Bone.py
@@ -7,3 +7,5 @@ class WarCraft3Bone(WarCraft3Node):
     def __init__(self):
         super().__init__('bone')
         self.geoset_id: Optional[int] = None
+        self.geoanimation_id: Optional[int] = None
+
